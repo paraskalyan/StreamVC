571f9d6de28dbdf7eea498b29c320defd627f650
Paras Kalyan <paraskalyan20@gmail.com>
2025-06-07 22:54:21 +0530

    Onboard API and Token verification middleware implemented

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
-
+import cookieParser from "cookie-parser";
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  onboard,
+  signup,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/logout", logout);
+router.post("/onboard", protectRoute, onboard);
 
 export default router;
