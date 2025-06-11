dcbbf1ce56c0407864cc5442fa6a02bdbcfebab8
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2025-06-11 18:20:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use("/api/chat", chatRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
